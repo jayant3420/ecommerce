922b52d00e4218b0f9732030a09b5c0ee7dec77c
jayant kumar <jayant.3420@gmail.com>
2021-08-06 21:36:13 +0530

    third commit

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use("/navdata", nav);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, console.log(`server running at port ${PORT}`));
